b7b1f05df5b9593d7d54e3b1aa807a9f5477ba4e
Ashish Yadav <ashish002145@gmail.com>
2024-06-15 15:35:03 +0530

    updated schemas/__init__.py for defining data structure for user and s3_credentials

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,13 +1,18 @@
 from pydantic import BaseModel
 
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    
 class UserLogin(BaseModel):
     username: str
     password: str
     
-class S3Credentials(BaseModel):
-    accessKeyId: str
-    secretAccessKey: str
+class S3CredentialsCreate(BaseModel):
+    access_key_id: str
+    secret_access_key: str
     
 class S3Item(BaseModel):
-    itemName: str
-    itemContent: str
+    item_name: str
+    item_content: str

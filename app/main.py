65fb1205dce65761f3e7745d41e15caf0d9c064a
Ashish Yadav <ashish002145@gmail.com>
2024-06-16 16:42:42 +0530

    modified main.py file for s3 api integration

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api import auth
+from app.api import auth, s3
 from app.models import user
 from app.core.database import engine
 import logging
@@ -13,7 +13,7 @@ user.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth")
-# app.include_router(s3.router, prefix="/s3")
+app.include_router(s3.router, prefix="/s3")
 
 @app.get("/")
 def read_root():
